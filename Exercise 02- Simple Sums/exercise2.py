3df2a9df4f11c099f5fe1dbf8f017dbe582a98b3
Ayman-ali21 <Aymanalishafeeque21@gmail.com>
2025-11-24 09:47:04 +0400

    Add files via upload

---
+++ Exercise 02- Simple Sums/exercise2.py
@@ -0,0 +1,8 @@
+# Define the first number
+number1 = 8
+# Define the second number
+number2 = 10
+# Calculate the sum of number1 and number2
+sum_result = number1 + number2
+# Print the result to the console
+print(sum_result)
